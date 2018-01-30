05c1ebde84525a52bbc1a9d3f27a72b427d89b50
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-30 16:26:45 +0200

    Add both starting date and time and ending date and time for event

--- src/components/NewEvent.js
+++ src/components/NewEvent.js
@@ -2,10 +2,10 @@ import React from "react";
 import Dialog from "material-ui/Dialog";
 import FlatButton from "material-ui/FlatButton";
 import RaisedButton from "material-ui/RaisedButton";
-import DatePicker from "material-ui/DatePicker";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+
 
 /**
  * Dialogs can be nested. This example opens a Date Picker from within a Dialog.
@@ -15,7 +15,10 @@ export default class NewEvent extends React.Component {
   state = {
     open: false,
     eventTitle: "",
-    eventDate: "",
+    eventStartDate: "",
+    eventStartTime: "",
+    eventEndDate: "",
+    eventEndTime: "",
     eventType: "",
     eventInformation: ""
   };
@@ -25,6 +28,10 @@ export default class NewEvent extends React.Component {
   };
 
   handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleCloseAndSend = () => {
     this.setState({ open: false });
 
     const data = {
@@ -36,51 +43,71 @@ export default class NewEvent extends React.Component {
       //TODO kirjautuneen käyttäjän yhdistäminen eventiin"
       organizerName: "Partio",
       organizerId: 2
-    }
+    };
 
-    fetch('http://localhost:3001/events', {
-      method: 'POST',
-      body: JSON.stringify(data), 
+    fetch("http://localhost:3001/events", {
+      method: "POST",
+      body: JSON.stringify(data),
       headers: new Headers({
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       })
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
+    })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response));
   };
 
-  handleDate = (event, date) => {
+  handleStartDate = (event) => {
     this.setState({
-      eventDate: date
-    })
-  }
+      eventStartDate: event.target.value
+    });
+  };
 
-  handleTitle = (event) => {
-    console.log(event)
+  handleStartTime = (event) => {
+    this.setState({
+      eventStartTime: event.target.value
+    });
+  };
+
+  handleEndDate = (event) => {
+    this.setState({
+      eventEndDate: event.target.value
+    });
+  };
+
+  handleEndTime = (event) => {
+    this.setState({
+      eventEndTime: event.target.value
+    });
+  };
+
+  handleTitle = event => {
+    console.log(event);
     this.setState({
       eventTitle: event.target.value
-    })
-  }
-
-  handleType = (event, index, type) => { 
-    this.setState({ 
-      eventType: type
-     })
+    });
   };
 
-  handleInformation = (event) => {
+  handleType = (event, index, type) => {
+    this.setState({
+      eventType: type
+    });
+  };
+
+  handleInformation = event => {
     this.setState({
       eventInformation: event.target.value
-    })
-  }
+    });
+  };
 
   render() {
     const actions = [
+      <FlatButton label="Peruuta" primary={true} onClick={this.handleClose} />,
       <FlatButton
-        label="Ok"
+        label="Tallenna"
         primary={true}
         keyboardFocused={true}
-        onClick={this.handleClose}
+        onClick={this.handleCloseAndSend}
       />
     ];
 
@@ -93,8 +120,35 @@ export default class NewEvent extends React.Component {
           modal={false}
           open={this.state.open}
           onRequestClose={this.handleClose}
+          autoScrollBodyContent={true}
         >
-          <DatePicker hintText="Valitse päivämäärä" onChange={this.handleDate} />
+          <p>Aloitus päivämäärä ja aika</p>
+          <TextField
+            type="date"
+            name="startDate"
+            value={this.state.eventStartDate}
+            onChange={this.handleStartDate}
+          />
+          <TextField
+            type="time"
+            name="startTime"
+            value={this.state.time}
+            onChange={this.handleStartTime}
+          />
+          <p>Lopetus päivämäärä ja aika</p>
+          <TextField
+            type="date"
+            name="endDate"
+            value={this.state.eventEndDate}
+            onChange={this.handleEndDate}
+          />
+          <TextField
+            type="time"
+            name="endTime"
+            value={this.state.eventEndTime}
+            onChange={this.handleEndTime}
+          />
+
           <TextField hintText="Tapahtuman nimi" onChange={this.handleTitle} />
           <br />
           <SelectField
@@ -108,10 +162,9 @@ export default class NewEvent extends React.Component {
             <MenuItem value={"vaellus"} primaryText="Vaellus" />
             <MenuItem value={"muu tapahtuma"} primaryText="Muu tapahtuma" />
           </SelectField>
-
           <TextField
             hintText="Message Field"
-            floatingLabelText="Lisätietoja" 
+            floatingLabelText="Lisätietoja"
             onChange={this.handleInformation}
             multiLine={true}
             rows={2}
