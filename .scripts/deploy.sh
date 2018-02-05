cc2f1634e254215355e2fe6f7f05d457d55cbf5a
Kari Vänttinen <kvanttinen@gmail.com>
2018-02-05 10:00:06 +0200

    key added to travis and scripts finalized

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,10 +4,6 @@ eval "$(ssh-agent -s)"
 chmod 600 ~/.travis/id_rsa
 ssh-add ~/.travis/id_rsa
 
-#git config --global push.default matching
-#git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-#git push deploy master
-
 ssh deploy@$IP -p $PORT <<EOF
     cd /var/www/
     git clone https://github.com/partio-scout/tosu-frontend.git
