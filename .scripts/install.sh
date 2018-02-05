4f30a8eec54bd8766d3224950a50afb67baa07ad
Kari Vänttinen <kvanttinen@gmail.com>
2018-02-05 10:03:03 +0200

    key added to travis and scripts finalized

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-openssl aes-256-cbc -K $encrypted_1f34a7791e59_key -iv $encrypted_1f34a7791e59_iv in deploy.pem.enc -out deploy.pem -d
+openssl aes-256-cbc -K $encrypted_1f34a7791e59_key -iv $encrypted_1f34a7791e59_iv -in deploy.pem.enc -out deploy.pem -d
 rm deploy.pem.enc
 chmod 600 deploy.pem
 mv deploy.pem ~/.travis/id_rsa
