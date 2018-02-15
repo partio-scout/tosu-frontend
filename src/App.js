daa0961a90cf3fb5fb19f1fcd5073e7ccfeb76a3
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-15 18:51:05 +0200

    Fetch activites from backend, use static JSON file if backend is down

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
-import React, { Component } from "react";
-import ListEvents from "./components/ListEvents";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import NewEvent from "./components/NewEvent";
-import Activities from "./components/Activities";
-import activitiesData from "./partio.json";
-import Appbar from "./components/AppBar";
+import React, { Component } from 'react'
+import ListEvents from './components/ListEvents'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import NewEvent from './components/NewEvent'
+import Activities from './components/Activities'
+import Appbar from './components/AppBar'
+import activitiesData from './partio.json'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      events: [{}]
-    };
-    this.getEvents = this.getEvents.bind(this);
+      events: [{}],
+      activities: undefined
+    }
+    this.getEvents = this.getEvents.bind(this)
+    this.getActivities = this.getActivities.bind(this)
   }
 
   getEvents = () => {
     fetch(
-      "https://cors-anywhere.herokuapp.com/https://suunnittelu.partio-ohjelma.fi:3001/events"
+      'https://cors-anywhere.herokuapp.com/https://suunnittelu.partio-ohjelma.fi:3001/events'
     )
       .then(res => res.json())
       .then(data => this.setState({ events: data }))
       .catch(error => {
-        console.log(error);
+        console.log(error)
         this.setState({
-          events: [{
-            title: "Backend not working, fake data to prevent error",
-            startDate: "2018-02-06",
-            startTime: "11:43",
-            endDate: "2018-02-09",
-            endTime: "13:43",
-            type: "kokous",
-            information: "oooooo",
-            id: "1"
-          }]
-        });
-      });
-  };
+          events: [
+            {
+              title: 'Backend not working, fake data to prevent error',
+              startDate: '2018-02-06',
+              startTime: '11:43',
+              endDate: '2018-02-09',
+              endTime: '13:43',
+              type: 'kokous',
+              information: 'oooooo',
+              id: '1'
+            }
+          ]
+        })
+      })
+  }
 
-  componentWillMount() {
-    this.getEvents();
+  getActivities = () => {
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://suunnittelu.partio-ohjelma.fi:3001/pofdata'
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          activities: data
+        })
+      })
+      .catch(error => {
+        console.log('Error: ', error)
+        //Jos tietoja ei saada haettua, hae tiedot staattisesta JSON-tiedostosta
+        this.setState({
+          activities: activitiesData
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.getEvents()
+    this.getActivities()
   }
 
   updateEvents = () => {
-    this.getEvents();
-  };
+    this.getEvents()
+  }
 
   render() {
     return (
@@ -57,13 +81,13 @@ class App extends Component {
               <NewEvent updateEvents={this.updateEvents} />
               <ListEvents events={this.state.events} />
               <h2>Activities</h2>
-              <Activities data={activitiesData} />
+              <Activities fetchedActivities={this.state.activities} />
             </div>
           </div>
         </MuiThemeProvider>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
