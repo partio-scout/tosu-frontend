06ebbcf05b7be68d31dec5bcaa652029babf5d4e
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-03 15:26:29 +0200

    fixed hostname if value

--- src/api-config.js
+++ src/api-config.js
@@ -10,15 +10,11 @@ console.log('window', window)
 console.log('windowloc', window.location)
 console.log('winlochost', window.location.hostname)
 
-if (hostname === 'https://suunnittelu.partio-ohjelma.fi') {
+if (hostname === 'suunnittelu.partio-ohjelma.fi') {
 
   backendHost = 'https://suunnittelu.partio-ohjelma.fi:3001';
 
-} else if (hostname === 'https://localhost:3001') {
-
-  backendHost = 'https://localhost:3001';
-
-} else {
+}  else {
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
 }
 
