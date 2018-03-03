f8f704c38bc7cb8c8c0fe50f89b5c06e59e8e161
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-03 15:18:26 +0200

    console logs added to find out hostname

--- src/api-config.js
+++ src/api-config.js
@@ -2,8 +2,22 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === 'https://suunnittelu.partio-ohjelma.fi') {
+
+console.log('------')
+console.log(hostname)
+console.log('------')
+console.log('window', window)
+console.log('windowloc', window.location)
+console.log('winlochost', window.location.hostname)
+
+if (hostname === 'https://suunnittelu.partio-ohjelma.fi') {
+
   backendHost = 'https://suunnittelu.partio-ohjelma.fi:3001';
+
+} else if (hostname === 'https://localhost:3001') {
+
+  backendHost = 'https://localhost:3001';
+
 } else {
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
 }
