9b6b7c0647fd83145a71774758513e2161e39e5a
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2018-03-08 13:20:20 +0000

    Revert "Searchbar filterv3"

--- src/functions/searchBarFiltering.js
+++ src/functions/searchBarFiltering.js
@@ -1,15 +1,9 @@
 
 const filterOffExistingOnes = (source, events, buffer) => {
   let toFilter = source;
-
-  //combine into 1 array
   let combinedEventActivities = events.map(event => event.activities);
   combinedEventActivities = [].concat.apply([], combinedEventActivities);
-  if (buffer !== undefined) {
-    combinedEventActivities = [].concat.apply(combinedEventActivities, buffer.activities)
-  } 
 
-  //nothing to filter
   if (combinedEventActivities[0] === undefined) {
     return toFilter;
   }
