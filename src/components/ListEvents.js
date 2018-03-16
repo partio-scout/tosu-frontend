0941613aa57842b7348fcf365012d2afa134a80e
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2018-03-16 17:42:22 +0200

    added sort to events

--- src/components/ListEvents.js
+++ src/components/ListEvents.js
@@ -2,8 +2,18 @@ import React from 'react'
 import EventCard from './EventCard'
 import { connect } from 'react-redux'
 
+const compare = (a, b) => {
+  if(a.startDate < b.startDate) {
+    return -1
+  } else if (b.startDate < a.startDate) {
+    return 1
+  }
+  return 0
+}
+
 const ListEvents = props => {
-  const listOfEvents = props.events.map(event => (
+  const sortedEvents = props.events.sort(compare)
+  const listOfEvents = sortedEvents.map(event => (
     <EventCard
       key={event.id ? event.id : 0}
       event={event}
