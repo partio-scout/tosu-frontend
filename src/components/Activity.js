f684b21a764fb89ffe9a92fb27951e84921c9b78
Jenni Nurmi <jenniknurmi@gmail.com>
2018-03-16 13:00:53 +0200

    Create different css for mandatory activity

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Avatar from 'material-ui/Avatar';
+import React from 'react'
+import Avatar from 'material-ui/Avatar'
 import Chip from 'material-ui/Chip'
-import { blue300, indigo900 } from 'material-ui/styles/colors'
+import { blue300, red300, indigo900, red900 } from 'material-ui/styles/colors'
 import { deleteActivityFromEvent } from '../reducers/eventReducer'
 import { deleteActivityFromBuffer } from '../reducers/bufferZoneReducer'
 import { connect } from 'react-redux'
@@ -17,50 +17,65 @@ const styles = {
     color: blue300,
     backgroundColor: indigo900,
     margin: 4
+  },
+  chipMandatory: {
+    margin: 4,
+    float: 'left',
+    backgroundColor: red300
+  },
+  avatarMandatory: {
+    size: 28,
+    color: red300,
+    backgroundColor: red900,
+    margin: 4
   }
-};
+}
 const handleRequestDelete = async (activity, props) => {
   try {
-    if (props.buffer.activities.find(a => a.id.toString() === activity.id.toString()) !== undefined) {
+    if (
+      props.buffer.activities.find(
+        a => a.id.toString() === activity.id.toString()
+      ) !== undefined
+    ) {
       props.deleteActivityFromBuffer(activity.id)
-      
     } else {
       props.deleteActivityFromEvent(activity.id)
     }
-
   } catch (exception) {
-    console.error('Error in deleting activity:', exception);
+    console.error('Error in deleting activity:', exception)
   }
-};
+}
 
 const Activity = props => {
   const { activity, act } = props
   if (activity && act[0]) {
+    console.log('Activity', act[0])
+
+    if (act[0].mandatory) {
+      return (
+        <Chip
+          onRequestDelete={() => handleRequestDelete(activity, props)}
+          style={styles.chipMandatory}
+          key={activity.id}
+        >
+          <Avatar style={styles.avatarMandatory}>!</Avatar>
+          <span className="activityTitle">{act[0].title}</span>
+        </Chip>
+      )
+    }
     return (
       <Chip
         onRequestDelete={() => handleRequestDelete(activity, props)}
         style={styles.chip}
         key={activity.id}
       >
-        <Avatar
-          style={styles.avatar}
-        >
-          !
-        </Avatar>
+        <Avatar style={styles.avatar}>!</Avatar>
         <span className="activityTitle">{act[0].title}</span>
       </Chip>
-    );
-
+    )
   }
-  return (
-    <div />
-  );
-
-
-
-
-
-};
+  return <div />
+}
 
 const mapStateToProps = state => {
   return {
@@ -68,8 +83,7 @@ const mapStateToProps = state => {
     buffer: state.buffer
   }
 }
-export default connect(
-  mapStateToProps,
-  { deleteActivityFromEvent, deleteActivityFromBuffer }
-
-)(Activity)
+export default connect(mapStateToProps, {
+  deleteActivityFromEvent,
+  deleteActivityFromBuffer
+})(Activity)
