a4722224dc7f9901937ddc74672b1e7222ec50f2
kapistelijaKrisu <krisuppi@yahoo.com>
2018-04-12 11:19:04 +0300

    unused components removed, the rest

--- src/services/pof.js
+++ src/services/pof.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
 import { POF_ROOT } from '../api-config';
 
-const getAll = async () => {
-  const response = await axios.get(`${POF_ROOT}/pofdata/tarppo`)
-  return response.data
-}
-
 const getAllTree = async () => {
   const response = await axios.get(`${POF_ROOT}/filledpof/tarppo`)
   return response.data
 }
 
-export default { getAll, getAllTree }
+export default { getAllTree }
