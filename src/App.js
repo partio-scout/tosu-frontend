fbcf357fd09e01b6067753152f398dc0bf76ea71
Michael <micaminoff@gmail.com>
2018-09-20 13:54:35 +0300

    Fixed event button clickability

--- src/App.js
+++ src/App.js
@@ -240,11 +240,13 @@ class App extends Component {
 
               <div id="container" style={{ paddingTop: padding }}>
                 <div className="content">
-                  <Button component={Link} to="/" onClick={this.openTopBar} variant="contained">
+                  <Button component={Link} to="/" onClick={this.openTopBar} variant="contained" className='priority-z'>
                     Lista tapahtumista
                   </Button>
                   &nbsp;
-                  <Button component={Link} to="/new-event" onClick={this.hideTopBar} variant="contained">
+                  <Button component={Link} to="/new-event" onClick={this.hideTopBar} variant="contained"
+                    className='priority-z'
+                  >
                     Uusi tapahtuma
                   </Button>
                   &nbsp;
