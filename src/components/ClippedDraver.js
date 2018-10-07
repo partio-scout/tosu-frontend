600a72348ebfc356754235dfecf38c441e9bbd58
Michael Aminoff <micaminoff@gmail.com>
2018-10-07 19:03:11 +0300

    Fix status message closing and opening

--- src/components/ClippedDraver.js
+++ src/components/ClippedDraver.js
@@ -1,72 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import TreeSearchBar from './TreeSearchBar'
 import BufferZone from './BufferZone'
 import StatusMessage from './StatusMessage'
 
 
-const drawerWidth = 500;
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: '100%',
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    minWidth: 0, // So the Typography noWrap works
-  }
-});
-
-function ClippedDrawer(props) {
-  const { classes } = props;
-  let showStatusBox = true;
-
-  const handleOpen = () => {
-    showStatusBox = true
+class ClippedDrawer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { showStatusBox: true }
   }
 
-  const handleClose = () => {
-    showStatusBox = false
+  handleOpen = () => {
+    this.setState({ showStatusBox: true })
   }
 
-  return (
-    <div className={classes.root}>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <StatusMessage
-          showStatusBox={showStatusBox}
-          handleClose={handleClose}
-          handleOpen={handleOpen}
-        />
-        <TreeSearchBar />
-        <BufferZone />
-      </Drawer>
-    </div>
-  )
+  handleClose = () => {
+    this.setState({ showStatusBox: false })
+  }
+
+  render() {
+    return (
+      <div className='drawer-root'>
+        <Drawer
+          variant="permanent"
+          className='drawer-paper'
+        >
+          <StatusMessage
+            showStatusBox={this.state.showStatusBox}
+            handleClose={this.handleClose}
+            handleOpen={this.handleOpen}
+          />
+          <TreeSearchBar />
+          <BufferZone />
+        </Drawer>
+      </div>
+    )
+  }
 }
 
-ClippedDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(ClippedDrawer);
+export default ClippedDrawer;
