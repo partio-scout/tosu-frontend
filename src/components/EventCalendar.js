87bf8e792c06718703ce649e0e9a92708f4c50cf
virtalas <virtalas@users.noreply.github.com>
2018-10-17 19:08:15 +0300

    material ui modal

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
+import Modal from '@material-ui/core/Modal'
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -40,6 +41,44 @@ function createActivityMarkers(activities) {
   return markers
 }
 
+class CustomEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    };
+  }
+
+  handleOpen = () => {
+    this.setState({ isModalOpen: true });
+  };
+
+  handleClose = () => {
+    this.setState({ isModalOpen: false });
+  };
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <div onClick={this.handleOpen}>
+          <b>whatever for events</b>
+        </div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.isModalOpen}
+          onClose={this.handleClose}
+        >
+          <div>
+            <span>in popup</span>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
 export default class EventCalendar extends Component {
 
   render() {
@@ -48,16 +87,88 @@ export default class EventCalendar extends Component {
     return (
       <div className="event-calendar">
         <BigCalendar
+          popup
           localizer={localizer}
           events={prepareEvents(events)}
           startAccessor="start"
           endAccessor="end"
           showMultiDayTimes
           components={{
-            event: Event,
+            event: CustomEvent,
           }}
         />
       </div>
     )
   }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isAddModalOpen: false,
+  //     isEditModalOpen: false,
+  //   };
+  // }
+  //
+  // toggleAddModal = event => {
+  //   if (!this.state.isEditModalOpen) {
+  //     this.setState({
+  //       currentEvent: event,
+  //       isAddModalOpen: !this.state.isAddModalOpen,
+  //     });
+  //   }
+  // };
+  // toggleEditModal = event => {
+  //   if (!this.state.isAddModalOpen) {
+  //     this.setState({
+  //       currentEvent: event,
+  //       isEditModalOpen: !this.state.isEditModalOpen,
+  //     });
+  //   }
+  // };
+  // render() {
+  //   const { events } = this.props
+  //
+  //   return (
+  //     <div className="event-calendar">
+  //       <BigCalendar
+  //         selectable
+  //         onSelectSlot={this.toggleAddModal}
+  //         onSelectEvent={this.toggleEditModal}
+  //         localizer={localizer}
+  //         events={prepareEvents(events)}
+  //         // Any other props you are using
+  //       />
+  //       <Modal
+  //         aria-labelledby="simple-modal-title"
+  //         aria-describedby="simple-modal-description"
+  //         open={this.state.isEditModalOpen}
+  //         onClose={this.toggleEditModal}
+  //       >
+  //         <div>
+  //           <span>in popup</span>
+  //         </div>
+  //       </Modal>
+  //       <Modal
+  //         aria-labelledby="simple-modal-title"
+  //         aria-describedby="simple-modal-description"
+  //         open={this.state.isAddModalOpen}
+  //         onClose={this.toggleAddModal}
+  //
+  //       >
+  //         <div>
+  //           <span>not in popup</span>
+  //         </div>
+  //       </Modal>
+  //       // <Modal open={this.state.isAddModalOpen} toggle={this.toggleAddModal}>
+  //       //     // whatever is displayed when you click the calendar
+  //       // </Modal>
+  //       // <Modal open={this.state.isEditModalOpen} toggle={this.toggleEditModal}>
+  //       //     // whatever is displayed when you click an event
+  //       //     <div>
+  //       //       <span>in popup</span>
+  //       //     </div>
+  //       // </Modal>
+  //     </div>
+  //   );
+  // }
 }
