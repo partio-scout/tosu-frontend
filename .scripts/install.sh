114c2aafdb0fd54d9209574ba91fb64500fbddba
hyytiala <otto.hyytiala@gmail.com>
2018-10-18 15:04:21 +0300

    New pipeline scripts

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-openssl enc -nosalt -aes-256-cbc -d -in tosu_node.pem.enc -out tosu_node.pem -base64 -K $encrypted_key -iv $encrypted_iv
-rm tosu_node.pem.enc
+echo "$PRIVATE_KEY" > tosu_node.pem
 chmod 600 tosu_node.pem
 mv tosu_node.pem ~/.travis/id_rsa
