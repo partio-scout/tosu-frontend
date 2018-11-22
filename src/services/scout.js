a1259b2b7b1ec4d7ccfc0541d490ef8995a796e2
virtalas <virtalas@users.noreply.github.com>
2018-11-22 16:20:27 +0200

    Update google urls

--- src/services/scout.js
+++ src/services/scout.js
@@ -7,12 +7,12 @@ const deleteScout = async (scoutId,token) => {
 }
 
 const findOrCreateScout = async (token) => {
-  const response = await axios.post(`${API_ROOT}/scouts`, {Authorization: token})
+  const response = await axios.post(`${API_ROOT}/scouts/google/login`, {Authorization: token})
   return response.data
 }
 
 const logout = async () => {
-  const response = await axios.post(`${API_ROOT}/scouts/logout`, {})
+  const response = await axios.post(`${API_ROOT}/scouts/google/logout`, {})
   return response.data
 }
 
