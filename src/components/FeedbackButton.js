8013a962b99e5a858181638d4c2074789e821d7f
Vili Lipo <vili.lipo@helsinki.fi>
2019-01-18 14:06:59 +0200

    changed to stateles version, and linted

--- src/components/FeedbackButton.js
+++ src/components/FeedbackButton.js
@@ -2,29 +2,20 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
-const styles = theme => ({
+const styles = () => ({
   label: {
     color: 'white'
   }
 })
 
-class FeedbackButton extends React.Component {
-  constructor(props) {
-    super(props)
-
-  }
-
-  render(props) {
-    return (
-      <div className='feedback'>
-        <a style={{textDecoration: 'none'}} href='https://docs.google.com/forms/d/e/1FAIpQLSddXqlQaFd8054I75s4UZEPeQAh_ardxRl11YYw3b2JBk0Y-Q/viewform'>
-        <Button type="button" className="button" variant='contained'color='secondary'>
-         Anna palautetta
-         </Button>
-        </a>
-      </div>
-    )
-  }
-}
+const FeedbackButton = () => (
+  <div className='feedback'>
+    <a style={{textDecoration: 'none'}} href='https://docs.google.com/forms/d/e/1FAIpQLSddXqlQaFd8054I75s4UZEPeQAh_ardxRl11YYw3b2JBk0Y-Q/viewform'>
+      <Button type="button" className="button" variant='contained'color='secondary'>
+        Anna palautetta
+      </Button>
+    </a>
+  </div>
+);
 
 export default withStyles(styles)(FeedbackButton)
