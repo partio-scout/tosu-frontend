bd8f49002b52218390a342d7d65cb979d8758556
Jussi Steenari <jussi.steenari@helsinki.fi>
2019-01-21 17:42:13 +0200

    css refactoring

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import axios from 'axios'
 import './index.css'
+
+import'./stylesheets/activity.css'
+import'./stylesheets/button.css'
+import'./stylesheets/calendar.css'
+import'./stylesheets/chip.css'
+import'./stylesheets/container.css'
+import'./stylesheets/content.css'
+import'./stylesheets/drawer.css'
+import'./stylesheets/eventcard.css'
+import'./stylesheets/footer.css'
+import'./stylesheets/form.css'
+import'./stylesheets/icons.css'
+import'./stylesheets/login.css'
+import'./stylesheets/mobile.css'
+import'./stylesheets/search.css'
+import'./stylesheets/statusbox.css'
+import'./stylesheets/tooltip.css'
+import'./stylesheets/utility.css'
+
+
+
+
 import App from './App'
 import store from './store'
 import pofService from './services/pof'
