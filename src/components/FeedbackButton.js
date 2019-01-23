d68004591c09e2f4c4239024f83a7c3f47dcedcb
Vili Lipo <vili.lipo@helsinki.fi>
2019-01-23 15:31:28 +0200

    added proptypes and changed the color of the feedbackButton

--- src/components/FeedbackButton.js
+++ src/components/FeedbackButton.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
 const styles = () => ({
-  label: {
-    color: 'white'
+    root: {
+    background: 'white',
+    borderRadius: 3,
+    border: '1px solid gray',
+    color: 'black'
   }
 })
+
+const StyledButton = withStyles (styles)(Button)
 
 const FeedbackButton = (props) => (
   <div className='feedback'>
     <a style={{textDecoration: 'none'}} href={props.feedback_url}>
-      <Button type="button" className="button" variant='contained'color='secondary'>
+      <StyledButton type="button" className="FeedbackButton" variant='contained'>
         Anna palautetta
-      </Button>
+      </StyledButton>
     </a>
   </div>
 );
 
-export default withStyles(styles)(FeedbackButton)
+export default (FeedbackButton)
+
+FeedbackButton.propTypes = {
+    feedback_url: PropTypes.string.isRequired
+}
+
+
