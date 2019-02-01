60ff5e69deac1defc2f0b754f12bc09583bea854
Vili Lipo <vili.lipo@helsinki.fi>
2019-02-01 13:33:21 +0200

    tests

--- src/components/ActivityDragAndDropTarget.js
+++ src/components/ActivityDragAndDropTarget.js
@@ -4,10 +4,18 @@ import { DropTarget } from 'react-dnd'
 import PropTypes from 'prop-types'
 import ItemTypes from '../ItemTypes'
 import DropActivity from '../functions/DropActivity'
-import { notify } from '../reducers/notificationReducer' 
+import { notify } from '../reducers/notificationReducer'
 import { pofTreeUpdate } from '../reducers/pofTreeReducer'
-import { postActivityToBufferOnlyLocally, deleteActivityFromBufferOnlyLocally, deleteActivityFromBuffer } from '../reducers/bufferZoneReducer'
-import { deleteActivityFromEvent,  deleteActivityFromEventOnlyLocally, addActivityToEventOnlyLocally } from '../reducers/eventReducer'
+import {
+  postActivityToBufferOnlyLocally,
+  deleteActivityFromBufferOnlyLocally,
+  deleteActivityFromBuffer,
+} from '../reducers/bufferZoneReducer'
+import {
+  deleteActivityFromEvent,
+  deleteActivityFromEventOnlyLocally,
+  addActivityToEventOnlyLocally,
+} from '../reducers/eventReducer'
 
 function collect(connector, monitor) {
   return {
@@ -29,7 +37,7 @@ class ActivityDragAndDropTarget extends React.Component {
     // TODO: find a better solution
     bufferzone: PropTypes.bool.isRequired,
     parentId: PropTypes.number.isRequired,
-    
+
     notify: PropTypes.func.isRequired,
     deleteActivityFromEventOnlyLocally: PropTypes.func.isRequired,
     addActivityToEventOnlyLocally: PropTypes.func.isRequired,
@@ -42,9 +50,9 @@ class ActivityDragAndDropTarget extends React.Component {
 
   render() {
     const { isOver, canDrop, connectDropTarget, odd, event } = this.props
-    const baseColor = event ? odd ? '#EFEEEE' : '#D6E8F7' : '#FFF'
-    const background = { backgroundColor: (canDrop ? '#C8E6C9' : baseColor ) }
-    const className = (this.props.className || '')  + (isOver ? ' pattern' : '' )
+    const baseColor = event ? (odd ? '#EFEEEE' : '#D6E8F7') : '#FFF'
+    const background = { backgroundColor: canDrop ? '#C8E6C9' : baseColor }
+    const className = (this.props.className || '') + (isOver ? ' pattern' : '')
 
     return connectDropTarget(
       <div style={background} className={className}>
@@ -57,7 +65,7 @@ class ActivityDragAndDropTarget extends React.Component {
 const DroppableActivityDragAndDropTarget = DropTarget(
   ItemTypes.ACTIVITY,
   DropActivity,
-  collect,
+  collect
 )(ActivityDragAndDropTarget)
 
 const mapStateToProps = state => {
@@ -67,13 +75,16 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {
-  notify,
-  deleteActivityFromEventOnlyLocally,
-  addActivityToEventOnlyLocally,
-  deleteActivityFromBufferOnlyLocally,
-  postActivityToBufferOnlyLocally,
-  pofTreeUpdate,
-  deleteActivityFromBuffer,
-  deleteActivityFromEvent,
-})(DroppableActivityDragAndDropTarget)
+export default connect(
+  mapStateToProps,
+  {
+    notify,
+    deleteActivityFromEventOnlyLocally,
+    addActivityToEventOnlyLocally,
+    deleteActivityFromBufferOnlyLocally,
+    postActivityToBufferOnlyLocally,
+    pofTreeUpdate,
+    deleteActivityFromBuffer,
+    deleteActivityFromEvent,
+  }
+)(DroppableActivityDragAndDropTarget)
--- src/components/tests/Calendar.test.js
+++ src/components/tests/Calendar.test.js
@@ -4,31 +4,67 @@ import { expect } from 'chai'
 import { Calendar } from '../Calendar'
 
 const testPofTree = {}
-const testShowKuksa = true
 const testEvent = {
   title: 'tapahtuma',
-  start: '',
-  end: '',
+  startDate: '',
+  startTime: '',
+  endTime: '',
+  endDate: '',
+  activities: [],
+  type: '',
+  id: 1,
+  information: 'info',
+  synced: true,
+  kuksaEvent: false,
 }
-const testEvents = []
+
+const testKuksaEvent = {
+  title: 'Kuksa tapahtuma',
+  startDate: '',
+  startTime: '',
+  endTime: '',
+  endDate: '',
+  activities: [],
+  type: '',
+  id: 200,
+  information: 'info',
+  synced: true,
+  kuksaEvent: true,
+}
+
+const testEvents = [testEvent, testKuksaEvent]
 
 const mockClosePopper = jest.fn()
 const mobile = false
 
-const testCalendar = () => {
+const testCalendarNoKuksa = () => {
   return (
     <Calendar
       pofTree={testPofTree}
       events={testEvents}
-      shouldShowKuksaEventsAlso={testShowKuksa}
+      shouldShowKuksaEventsAlso={false}
       closePopper={mockClosePopper}
+      mobile={false}
     />
   )
 }
 
 describe('<Calendar />', () => {
-  it('renders', () => {
-    const wrapper = shallow(testCalendar())
-    console.log(wrapper.debug())
+  it('Kuksa events wont show if showKuksa is false', () => {
+    const wrapper = shallow(testCalendarNoKuksa())
+    const events = wrapper
+      .children()
+      .dive()
+      .instance().props.events
+    expect(
+      events.find(event => {
+        return event.id == testEvent.id
+      })
+    ).to.not.equal(undefined)
+    expect(
+      events.find(event => {
+        return event.id == testKuksaEvent.id
+      })
+    ).to.equal(undefined)
   })
 })
