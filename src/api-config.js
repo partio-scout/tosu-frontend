436a086b06e18951b6bbe6d6b165908c60347f31
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2019-02-18 07:21:11 +0000

    Update api-config.js

--- src/api-config.js
+++ src/api-config.js
@@ -1,16 +1,12 @@
 let root = 'http://localhost:3001'
-// root = 'https://suunnittelu.beta.partio-ohjelma.fi:3002'
 
 const hostname = window && window.location && window.location.hostname
 
-export const POF_ROOT = 'https://suunnittelu.partio-ohjelma.fi:3002'
-
 if (hostname === 'suunnittelu.beta.partio-ohjelma.fi') {
   root = 'https://suunnittelu.beta.partio-ohjelma.fi:3002'
-} else if (hostname === 'suunnittelu.partio-ohjelma.fi') {
-  root = 'https://suunnittelu.partio-ohjelma.fi:3002'
 }
 
 export const API_ROOT = root
+export const POF_ROOT = 'https://suunnittelu.beta.partio-ohjelma.fi:3002'
 
 export default { API_ROOT, POF_ROOT }
