a2205bf8bce62288aa106a39ef016e2bfdb289bc
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2019-03-03 17:54:22 +0200

    Remove unnecessary function wrapper

--- src/index.js
+++ src/index.js
@@ -5,18 +5,12 @@ import axios from 'axios'
 import './stylesheets/index.css'
 import App from './App'
 import store from './store'
-import { savePofData } from './services/localStorage'
-import { POF_ROOT } from './api-config'
 
 axios.defaults.withCredentials = true
 
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
-
-render()
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
