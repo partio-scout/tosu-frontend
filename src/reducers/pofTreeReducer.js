ea84e2a11eb1c2023add740348234df7bba10bc6
Jussi Steenari <jussi.steenari@helsinki.fi>
2019-04-17 12:53:09 +0300

    minor fixes

--- src/reducers/pofTreeReducer.js
+++ src/reducers/pofTreeReducer.js
@@ -53,7 +53,6 @@ const fillWithNeededVariable = root => {
     task.title = (
       <span
         name={task.title}
-        className="tree-search-title"
         style={{ backgroundColor: task.isMandatory ? '#2196f3' : '#E3F2FD' }}
       >
         {task.title}
