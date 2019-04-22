d35938bf9d55884fcc5d0c3151504542b762e2f9
leppaino <43615150+leppaino@users.noreply.github.com>
2019-04-22 07:33:18 +0300

    show activities, clear

--- cypress/integration/tarppo.spec.js
+++ cypress/integration/tarppo.spec.js
@@ -3,17 +3,27 @@ describe('After logging in', function() {
     cy.request('http://localhost:3001/scouts/testuser')
     cy.visit('http://localhost:3000')
   })
-  it('login succesful', function() {
-    //cy.contains('Teppo Testaaja')
+
+  it('', function() {
+    cy.visit('http://localhost:3000')
   })
+
   it('user can open the tarppo menu', function() {
-    /*cy.contains('Tervetuloa tarpojaksi').should('not.exist')
+    cy.contains('Tervetuloa tarpojaksi').should('not.exist')
     cy.contains('Valitse tarppo...').click({ force: true })
     cy.contains('Kaupunki-tarppo')
     cy.contains('Kaupunki-tarppo').click({ force: true })
     cy.contains('Lisää aktiviteetti').click({ force: true })
-    cy.contains('Kotiseutu').click({ force: true })*/
+    cy.contains('Kotiseutu').click({ force: true })
+  })
 
-    // Ei toimi koska valikko ei avaudu
+  // updates the screen and shows the activities
+  it('', function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('user can empty buffer', function() {
+    cy.contains('Tyhjennä')
+    cy.contains('Tyhjennä').click({ force: true })
   })
 })
