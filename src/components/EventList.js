4cd1496cb410b3175031b802e3fa8e84ab6f0782
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2019-04-22 20:09:24 +0300

    Delete old Tosu buttons

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import EventCard from './EventCard'
-import DeleteTosuButton from './DeleteTosuButton'
 import KuksaEventCard from './KuksaEventCard'
 import filterEvents from '../functions/filterEvents'
 import { eventList } from '../reducers/eventReducer'
@@ -20,30 +19,27 @@ const styles = theme => ({
 class EventList extends React.Component {
   render() {
     const view = this.props.ui.view
-    const { startDate, endDate, events, initialization, classes } = this.props
+    const { startDate, endDate, events, classes } = this.props
 
     const eventsToShow = () =>
       filterEvents(view, eventList(events), startDate, endDate)
 
     let odd = true
     return (
-      <React.Fragment>
-        <ul className={classes.eventList}>
-          {eventsToShow().map(event => {
-            odd = !odd
-            return (
-              <li key={event.id ? event.id : 0}>
-                {event.kuksaEvent ? (
-                  <KuksaEventCard event={event} />
-                ) : (
-                  <EventCard event={event} odd={odd} />
-                )}
-              </li>
-            )
-          })}
-        </ul>
-        <DeleteTosuButton initialization={initialization} />
-      </React.Fragment>
+      <ul className={classes.eventList}>
+        {eventsToShow().map(event => {
+          odd = !odd
+          return (
+            <li key={event.id ? event.id : 0}>
+              {event.kuksaEvent ? (
+                <KuksaEventCard event={event} />
+              ) : (
+                <EventCard event={event} odd={odd} />
+              )}
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
