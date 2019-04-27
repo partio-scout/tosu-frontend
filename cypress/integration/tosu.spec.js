6d8b9aeed3a4ffd9bf13af6a7ea1dc1e281f4df8
leppaino <43615150+leppaino@users.noreply.github.com>
2019-04-27 21:15:43 +0300

    waits removed

--- cypress/integration/tosu.spec.js
+++ cypress/integration/tosu.spec.js
@@ -2,26 +2,21 @@ describe('Tosu-menu', function() {
   beforeEach('user logs in', function() {
     cy.request('http://localhost:3001/scouts/testuser')
     cy.visit('http://localhost:3000')
-    cy.wait(4000)
   })
 
   it('a new tosu', function() {
     cy.get('button[id="tosu_selection"]').click()
-    cy.wait(4000)
     cy.get('input[placeholder="Uusi toimintasuunnitelma"]').type('myTosu', {
       multiple: true,
       force: true,
     })
     cy.get('button[id="plus_button"]').click()
     cy.get('button[id="tosu_selection"]').click()
-    cy.wait(10000)
     cy.contains('myTosu')
   })
 
   it('modify tosu', function() {
-    cy.wait(10000)
     cy.get('button[id="tosu_selection"]').click()
-    cy.wait(4000)
     cy.get('div[id="list_item"]')
       .first()
       .click({ force: true })
@@ -39,7 +34,6 @@ describe('Tosu-menu', function() {
 
   it('delete tosu', function() {
     cy.get('button[id="tosu_selection"]').click({ force: true })
-    cy.wait(10000)
     cy.get('div[id="list_item"]')
       .first()
       .click({ force: true })
@@ -48,10 +42,9 @@ describe('Tosu-menu', function() {
       .get('svg[id="delete_button"]')
       .click({ force: true })
     cy.get('button[id="confirm_remove"]').click({ force: true })
-    cy.wait(10000)
     cy.get('button[id="tosu_selection"]').click({ force: true })
-    cy.wait(4000)
     cy.get('input[placeholder="Uusi toimintasuunnitelma"]').should('exist')
     cy.get('button[id="close_button"]').click({ force: true })
   })
 })
+
