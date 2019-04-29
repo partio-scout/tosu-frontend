8cb2bb9646620e3f6a416dff19f95c6defcac5dc
leppaino <43615150+leppaino@users.noreply.github.com>
2019-04-29 17:39:56 +0300

    reset db

--- cypress/integration/tosu.spec.js
+++ cypress/integration/tosu.spec.js
@@ -1,7 +1,37 @@
+import scoutService from '../../src/services/scout'
+
+const resetDatabase = () => {
+  scoutService.deleteScout('12345')
+}
+
 describe('Tosu-menu', function() {
+  before('user logs in', function() {
+    resetDatabase()
+    cy.wait(10000)
+  })
+
   beforeEach('user logs in', function() {
+    cy.wait(4000)
     cy.request('http://localhost:3001/scouts/testuser')
     cy.visit('http://localhost:3000')
+  })
+
+  it('empty function', function() {})
+
+  it('delete tosu', function() {
+    cy.get('button[id="tosu_selection"]').click({ force: true })
+    cy.wait(10000)
+    cy.get('div[id="list_item"]')
+      .first()
+      .click({ force: true })
+    cy.get('div[id="action_buttons"]')
+      .first()
+      .get('svg[id="delete_button"]')
+      .click({ force: true })
+    cy.get('button[id="confirm_remove"]').click({ force: true })
+    cy.get('button[id="tosu_selection"]').click({ force: true })
+    cy.get('input[placeholder="Uusi toimintasuunnitelma"]').should('exist')
+    cy.get('button[id="close_button"]').click({ force: true })
   })
 
   it('a new tosu', function() {
@@ -47,4 +77,5 @@ describe('Tosu-menu', function() {
     cy.get('button[id="close_button"]').click({ force: true })
   })
 })
+
 
